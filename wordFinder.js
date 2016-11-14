bd9ad92a57e0dfedba217d2c0f6ff8f1e48d8ff0
Marcin Wróblewski <marcinwroblewski@outlook.com>
2016-11-14 22:37:32 +0100

    styles, fonts
    Nie oceniaj mnie za sposób dodania przedziałek między słowami w różnych
    językach :8ball:

--- wordFinder.js
+++ wordFinder.js
@@ -42,27 +42,45 @@ class WordFinder {
     }
 
     prepareResultNodes(node) {
-        let PL = document.createElement('div');
+        let row1 = document.createElement('div');
+        row1.setAttribute('class', 'row');
+
+        let PL = document.createElement('h4');
+        PL.setAttribute('class', 'col s4 center-align');
+        PL.setAttribute('style', 'border-right: 1px solid grey');
         PL.textContent = node.pl;
 
-        let EN = document.createElement('div');
+        let EN = document.createElement('h4');
+        EN.setAttribute('class', 'col s4 center-align');
+        EN.setAttribute('style', 'border-right: 1px solid grey');
         EN.textContent = node.en;
 
-        let CZ = document.createElement('div');
+        let CZ = document.createElement('h4');
+        CZ.setAttribute('class', 'col s4 center-align');
         CZ.textContent = node.cz;
 
+        row1.appendChild(PL);
+        row1.appendChild(EN);
+        row1.appendChild(CZ);
+
+        let row2 = document.createElement('div');
+        row2.setAttribute('class', 'row');
+
         let IMG = document.createElement('img');
+        IMG.setAttribute('class', 'col s6 center-align');
         IMG.setAttribute('src', node.imgPath);
 
         let SYMBOL = document.createElement('img');
+        SYMBOL.setAttribute('class', 'col s6 center-align');
         SYMBOL.setAttribute('src', node.symbol);
 
+        row2.appendChild(IMG);
+        row2.appendChild(SYMBOL);
+
         let resultNode = document.createElement('div');
-        resultNode.appendChild(PL);
-        resultNode.appendChild(EN);
-        resultNode.appendChild(CZ);
-        resultNode.appendChild(IMG);
-        resultNode.appendChild(SYMBOL);
+        resultNode.setAttribute('class', 'card ');
+        resultNode.appendChild(row1);
+        resultNode.appendChild(row2);
 
         return resultNode;
     }
