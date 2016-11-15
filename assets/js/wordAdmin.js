1ecf4444e5c2ba95c83d7b8369aa07371967ef5e
Grzegorz Mrózek <lpmusicon@gmail.com>
2016-11-15 12:31:51 +0100

    Tudududu

---
+++ assets/js/wordAdmin.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class WordAdmin
+{
+    constructor(dictionary, listElement) {
+        this.dictionary = dictionary;
+        this.listElement = listElement;
+        this.wordFinder = new WordFinder();
+    }
+
+    hashPassword(password) {
+        let hash = "";
+        return hash;
+    }
+
+}
